606d579a6429f402cbbad26e6fe70756946d675e
unknown <l.zientara@AMDC2566.DIGITAL.local>
2014-07-15 15:44:58 +0200

    pierwszy commit

---
+++ src/Sample.java
@@ -0,0 +1,6 @@
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("change to branch master");
+	}
+}
---
+++ src/com/samsung/sample/Extractor.java
@@ -0,0 +1,5 @@
+package com.samsung.sample;
+
+public class Extractor {
+
+}
